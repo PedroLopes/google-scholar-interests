72574c93ffa81f87cfb786981fe9e3b64db642b0
PedroLopes <pedrolopes@uchicago.edu>
2023-02-04 11:30:27 -0600

    histogram output

--- histogram.py
+++ histogram.py
@@ -1,3 +1,10 @@
+# This script opens a file with words on it (csv formatted)
+#   and will plot a histogram of those
+#   known workarounds: it takes in empty spaces for now
+#                      they should be ignored
+# Credit: modified from https://stackoverflow.com/questions/35596128/how-to-generate-a-word-frequency-histogram-where-bars-are-ordered-according-to
+
+
 from collections import Counter
 import csv
 import numpy as np
@@ -11,35 +18,27 @@ with open('cloud.csv', mode ='r')as file:
     for word in line:
        word_list.append(word)
      
-#word_list = ['A', 'A', 'B', 'B', 'A', 'C', 'C', 'C', 'C']
-
-
-
 counts = Counter(word_list)
-
 labels, values = zip(*counts.items())
-
 # sort your values in descending order
 indSort = np.argsort(values)[::-1]
-
 # rearrange your data
 labels = np.array(labels)[indSort]
 values = np.array(values)[indSort]
-
 indexes = np.arange(len(labels))
 
-print(indexes[0])
-print(labels[0])
-print(values[0])
+#print(indexes[0])
+#print(labels[0])
+#print(values[0])
 
-bar_width = 0.35
+bar_width = 0.5
 
+# skipping the first element of all because it's the "" (empty space) data
 indexes = indexes[1:]
 labels = labels[1:]
 values = values[1:]
 
+# let's plot
 plt.bar(indexes, values)
-
-# add labels
 plt.xticks(indexes + bar_width, labels, rotation='vertical')
 plt.show()
